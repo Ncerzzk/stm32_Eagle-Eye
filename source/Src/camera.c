005ae7b663f70a55840155b0b209c17fa3dc3bc1
ncer <huangzzk@bupt.edu.cn>
2018-07-05 11:29:07 +0800

    update

---
+++ source/Src/camera.c
@@ -0,0 +1,166 @@
+#include "camera.h"
+#include "i2c.h"
+#include "usart.h"
+
+
+IMG_State img_state=IMG_Start;
+
+extern DMA_HandleTypeDef hdma_tim8_ch4_trig_com;
+
+uint8_t camera_buffer[CAMERA_H*CAMERA_W/8];
+
+int camera_cnt=0;
+
+reg_s ov7725_eagle_reg[] =
+{
+    //寄存器，寄存器值次
+    {OV7725_COM4         , 0xC1},
+    {OV7725_CLKRC        , 0x00},
+    {OV7725_COM2         , 0x03},
+    {OV7725_COM3         , 0xD0},
+    {OV7725_COM7         , 0x40},
+    {OV7725_COM10        , 0x20},
+    {OV7725_HSTART       , 0x3F},
+    {OV7725_HSIZE        , 0x50},
+    {OV7725_VSTRT        , 0x03},
+    {OV7725_VSIZE        , 0x78},
+    {OV7725_HREF         , 0x00},
+    {OV7725_SCAL0        , 0x0A},
+    {OV7725_AWB_Ctrl0    , 0xE0},
+    {OV7725_DSPAuto      , 0xff},
+    {OV7725_DSP_Ctrl2    , 0x0C},
+    {OV7725_DSP_Ctrl3    , 0x00},
+    {OV7725_DSP_Ctrl4    , 0x00},
+
+#if (CAMERA_W == 80)
+    {OV7725_HOutSize     , 0x14},
+#elif (CAMERA_W == 160)
+    {OV7725_HOutSize     , 0x28},
+#elif (CAMERA_W == 240)
+    {OV7725_HOutSize     , 0x3c},
+#elif (CAMERA_W == 320)
+    {OV7725_HOutSize     , 0x50},
+#else
+
+#endif
+
+#if (CAMERA_H == 60 )
+    {OV7725_VOutSize     , 0x1E},
+#elif (CAMERA_H == 120 )
+    {OV7725_VOutSize     , 0x3c},
+#elif (CAMERA_H == 180 )
+    {OV7725_VOutSize     , 0x5a},
+#elif (CAMERA_H == 240 )
+    {OV7725_VOutSize     , 0x78},
+#else
+
+#endif
+
+    {OV7725_EXHCH        , 0x00},
+    {OV7725_GAM1         , 0x0c},
+    {OV7725_GAM2         , 0x16},
+    {OV7725_GAM3         , 0x2a},
+    {OV7725_GAM4         , 0x4e},
+    {OV7725_GAM5         , 0x61},
+    {OV7725_GAM6         , 0x6f},
+    {OV7725_GAM7         , 0x7b},
+    {OV7725_GAM8         , 0x86},
+    {OV7725_GAM9         , 0x8e},
+    {OV7725_GAM10        , 0x97},
+    {OV7725_GAM11        , 0xa4},
+    {OV7725_GAM12        , 0xaf},
+    {OV7725_GAM13        , 0xc5},
+    {OV7725_GAM14        , 0xd7},
+    {OV7725_GAM15        , 0xe8},
+    {OV7725_SLOP         , 0x20},
+    {OV7725_LC_RADI      , 0x00},
+    {OV7725_LC_COEF      , 0x13},
+    {OV7725_LC_XC        , 0x08},
+    {OV7725_LC_COEFB     , 0x14},
+    {OV7725_LC_COEFR     , 0x17},
+    {OV7725_LC_CTR       , 0x05},
+    {OV7725_BDBase       , 0x99},
+    {OV7725_BDMStep      , 0x03},
+    {OV7725_SDE          , 0x04},
+    {OV7725_BRIGHT       , 0x00},
+    {OV7725_CNST         , 0xFF},
+    {OV7725_SIGN         , 0x06},
+    {OV7725_UVADJ0       , 0x11},
+    {OV7725_UVADJ1       , 0x02},
+
+};
+
+extern DMA_HandleTypeDef hdma_memtomem_dma2_stream0;
+extern TIM_HandleTypeDef htim8;
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
+  TIM_HandleTypeDef * htim=&htim8;
+  
+
+  
+  if(img_state==IMG_Start){
+    if(GPIO_Pin==GPIO_PIN_8){
+      HAL_NVIC_DisableIRQ(EXTI9_5_IRQn);
+      HAL_DMA_Start_IT(&hdma_tim8_ch4_trig_com,(uint32_t)&(GPIOD->IDR),(uint32_t)camera_buffer,sizeof(camera_buffer));
+      __HAL_TIM_ENABLE_DMA(htim, TIM_DMA_CC4);
+    }
+  }
+}
+
+uint8_t sccb_write_byte(uint8_t address,uint8_t data){
+  return I2C_Write_Buffer(OV7725_ADDRESS,address,&data,1);
+}
+  
+uint8_t sccb_read_byte(uint8_t reg){
+  uint8_t data=0;
+  I2C_Read_Buffer(OV7725_ADDRESS,reg,&data,1);
+  return data;
+}
+
+void camera_init(){
+  int cfg_size;
+  uint8_t id;
+  uint8_t result=0xf1;
+  result=sccb_write_byte(OV7725_COM7,0x80);
+  uprintf("%d\r\n",result);
+  HAL_Delay(50);
+  id=sccb_read_byte(OV7725_VER);
+  if(id!=OV7725_ID){
+    //error!
+    uprintf("error read camera id:%d!\r\n",id);
+    return ;
+  }
+  
+  cfg_size=sizeof(ov7725_eagle_reg)/sizeof(reg_s);
+  
+  for(int i=0;i<cfg_size;++i){
+    result=sccb_write_byte(ov7725_eagle_reg[i].addr,ov7725_eagle_reg[i].val);
+    if(sccb_read_byte(ov7725_eagle_reg[i].addr)!=ov7725_eagle_reg[i].val){
+      uprintf("error!\r\n");
+    }
+  }
+  uprintf("successful write camera regs!\r\n");
+}
+
+void IMG_OK_Callback(DMA_HandleTypeDef *_hdma){
+  img_state=IMG_OK;
+  //uprintf("ok!\r\n");
+  //vcan_sendimg(camera_buffer,sizeof(camera_buffer));
+  //img_ok=0;
+}
+
+void vcan_sendimg(void *imgaddr, uint32_t imgsize)
+{
+    uint8_t cmdf[2] = {0x01, 0xFE};    //山外上位机 使用的命令
+    uint8_t cmdr[2] = {0xFE, 0x01};    //山外上位机 使用的命令
+
+    HAL_UART_Transmit(&huart2,cmdf, sizeof(cmdf),1000);
+    HAL_UART_Transmit(&huart2,(uint8_t *)imgaddr, imgsize,1000);
+    HAL_UART_Transmit(&huart2,cmdr, sizeof(cmdr),1000);
+}
+
+void get_img(){
+  img_state=IMG_Start;
+  //__HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_8);
+  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+
+}
---
+++ source/Src/tim.c
@@ -0,0 +1,185 @@
+/**
+  ******************************************************************************
+  * File Name          : TIM.c
+  * Description        : This file provides code for the configuration
+  *                      of the TIM instances.
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2018 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "tim.h"
+
+#include "gpio.h"
+#include "dma.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+TIM_HandleTypeDef htim8;
+DMA_HandleTypeDef hdma_tim8_ch4_trig_com;
+
+/* TIM8 init function */
+void MX_TIM8_Init(void)
+{
+  TIM_MasterConfigTypeDef sMasterConfig;
+  TIM_IC_InitTypeDef sConfigIC;
+
+  htim8.Instance = TIM8;
+  htim8.Init.Prescaler = 0;
+  htim8.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim8.Init.Period = 0;
+  htim8.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim8.Init.RepetitionCounter = 0;
+  if (HAL_TIM_IC_Init(&htim8) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim8, &sMasterConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
+  sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
+  sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
+  sConfigIC.ICFilter = 0;
+  if (HAL_TIM_IC_ConfigChannel(&htim8, &sConfigIC, TIM_CHANNEL_4) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+  
+  HAL_TIM_IC_Start(&htim8,TIM_CHANNEL_4);
+  
+      /* Enable the TIM Capture/Compare 4  DMA request */
+  __HAL_TIM_ENABLE_DMA(&htim8, TIM_DMA_ID_CC4);
+
+}
+
+extern void IMG_OK_Callback(DMA_HandleTypeDef *_hdma);
+void HAL_TIM_IC_MspInit(TIM_HandleTypeDef* tim_icHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+  if(tim_icHandle->Instance==TIM8)
+  {
+  /* USER CODE BEGIN TIM8_MspInit 0 */
+
+  /* USER CODE END TIM8_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_TIM8_CLK_ENABLE();
+  
+    /**TIM8 GPIO Configuration    
+    PC9     ------> TIM8_CH4 
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_9;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF3_TIM8;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    /* TIM8 DMA Init */
+    /* TIM8_CH4_TRIG_COM Init */
+    hdma_tim8_ch4_trig_com.Instance = DMA2_Stream7;
+    hdma_tim8_ch4_trig_com.Init.Channel = DMA_CHANNEL_7;
+    hdma_tim8_ch4_trig_com.Init.Direction = DMA_PERIPH_TO_MEMORY ;
+    hdma_tim8_ch4_trig_com.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_tim8_ch4_trig_com.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_tim8_ch4_trig_com.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_tim8_ch4_trig_com.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_tim8_ch4_trig_com.Init.Mode = DMA_NORMAL;
+    hdma_tim8_ch4_trig_com.Init.Priority = DMA_PRIORITY_HIGH;
+    hdma_tim8_ch4_trig_com.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+    if (HAL_DMA_Init(&hdma_tim8_ch4_trig_com) != HAL_OK)
+    {
+      _Error_Handler(__FILE__, __LINE__);
+    }
+
+    /* Several peripheral DMA handle pointers point to the same DMA handle.
+     Be aware that there is only one stream to perform all the requested DMAs. */
+    __HAL_LINKDMA(tim_icHandle,hdma[TIM_DMA_ID_CC4],hdma_tim8_ch4_trig_com);
+    //__HAL_LINKDMA(tim_icHandle,hdma[TIM_DMA_ID_TRIGGER],hdma_tim8_ch4_trig_com);
+    //__HAL_LINKDMA(tim_icHandle,hdma[TIM_DMA_ID_COMMUTATION],hdma_tim8_ch4_trig_com);
+
+    HAL_DMA_RegisterCallback(&hdma_tim8_ch4_trig_com,HAL_DMA_XFER_CPLT_CB_ID,IMG_OK_Callback);
+  /* USER CODE BEGIN TIM8_MspInit 1 */
+
+  /* USER CODE END TIM8_MspInit 1 */
+  }
+}
+
+
+void HAL_TIM_IC_MspDeInit(TIM_HandleTypeDef* tim_icHandle)
+{
+
+  if(tim_icHandle->Instance==TIM8)
+  {
+  /* USER CODE BEGIN TIM8_MspDeInit 0 */
+
+  /* USER CODE END TIM8_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_TIM8_CLK_DISABLE();
+  
+    /**TIM8 GPIO Configuration    
+    PC9     ------> TIM8_CH4 
+    */
+    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_9);
+
+    /* TIM8 DMA DeInit */
+    HAL_DMA_DeInit(tim_icHandle->hdma[TIM_DMA_ID_CC4]);
+    HAL_DMA_DeInit(tim_icHandle->hdma[TIM_DMA_ID_TRIGGER]);
+    HAL_DMA_DeInit(tim_icHandle->hdma[TIM_DMA_ID_COMMUTATION]);
+  /* USER CODE BEGIN TIM8_MspDeInit 1 */
+
+  /* USER CODE END TIM8_MspDeInit 1 */
+  }
+} 
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
